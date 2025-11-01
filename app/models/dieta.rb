3e2f776691eddf3d68c220344aae4c413911c303
GuiMeloDev03 <guilhermemelo270203@gmail.com>
2025-11-01 13:00:46 -0300

    feat: calculo de propriedas de refeicao

--- app/models/dieta.rb
+++ app/models/dieta.rb
@@ -1,10 +1,26 @@
 class Dieta < ApplicationRecord
   belongs_to :user
 
-  has_many :dia_dietas, dependent: :destroy
+  has_many :refeicoes, dependent: :destroy
 
   accepts_nested_attributes_for :refeicoes, allow_destroy: true
 
   validates :nome, presence: true
   validates :objetivo, presence: true
+
+  def total_calorias
+    refeicoes.sum(:calorias).to_f
+  end
+
+  def total_proteinas
+    refeicoes.sum(:proteinas).to_f
+  end
+
+  def total_carboidratos
+    refeicoes.sum(:carboidratos).to_f
+  end
+
+  def total_gorduras
+    refeicoes.sum(:gorduras).to_f
+  end
 end
